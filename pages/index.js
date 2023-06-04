3454689e627f080a00471247529c3d188527b5df
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-04 02:28:46 +0100

    Added some minor features...

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Home = () => {
   return (
     <>
+      <div className="products-heading">
+        <h2>Best Seller Goods</h2>
+        <p>Goods that most our Customers buy.</p>
+      </div>
+      <div className="products-container">
+        {["Product 1", "Product 2", "Product 12"]?.map((product) => product)}
+      </div>
     </>
   )
 }
