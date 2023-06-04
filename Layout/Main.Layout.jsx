a42da310554dd4f3e1cd7d7d16072dc4a83e2fa6
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-04 02:34:56 +0100

    Added Layout to the Peoject...

---
+++ Layout/Main.Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+
+import { Footer, Navbar } from "../Components/Index"
+
+const MainLayout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Head>
+                <title>Project Cinni</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className="main-container">
+                {children}
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    )
+}
+
+export default MainLayout
