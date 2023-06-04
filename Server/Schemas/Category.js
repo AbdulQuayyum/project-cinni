c04303e6344f132e0552d77a549275e9c21c99cb
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-04 01:53:16 +0100

    Updated Schemas added to The Server...

--- Server/Schemas/Category.js
+++ Server/Schemas/Category.js
@@ -1,6 +1,26 @@
 export default {
-    name: 'Category',
     title: 'Category',
-    type: 'string',
-    fields: [],
-};
+    name: 'Category',
+    type: 'document',
+    fields: [
+        {
+            title: 'Title',
+            name: 'Title',
+            type: 'string',
+            // validation: Rule => Rule.required().max(50)
+        },
+        {
+            title: 'Description',
+            name: 'Description',
+            type: 'text',
+            rows: 3
+        },
+        {
+            title: 'Parent Category',
+            name: 'ParentCategory',
+            type: 'reference',
+            to: [{ type: 'Category' }],
+            description: 'Select a parent category (optional)'
+        }
+    ]
+}
--- Server/Schemas/Order.js
+++ Server/Schemas/Order.js
@@ -1,6 +1,33 @@
 export default {
-    name: 'Order',
     title: 'Order',
-    type: 'string',
-    fields: [],
-};
+    name: 'Order',
+    type: 'document',
+    fields: [
+        {
+            title: 'User',
+            name: 'User',
+            type: 'reference',
+            to: [{ type: 'User' }],
+            // validation: Rule => Rule.required()
+        },
+        {
+            title: 'Products',
+            name: 'Products',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'Product' }] }],
+            // validation: Rule => Rule.required().min(1)
+        },
+        {
+            title: 'Total Amount',
+            name: 'TotalAmount',
+            type: 'number',
+            // validation: Rule => Rule.required().positive()
+        },
+        {
+            title: 'Order Date',
+            name: 'OrderDate',
+            type: 'datetime',
+            // validation: Rule => Rule.required()
+        }
+    ]
+}
