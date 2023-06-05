9c0258cbffddc1d97734ac44e1ba5fb4ec218b6b
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-06 00:58:46 +0100

    Implementing client side for server side...

--- Components/HeroBanner.jsx
+++ Components/HeroBanner.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import Link from 'next/link';
-import Image from 'next/image';
+
+import { UrlFor } from '@/Utilities/Client';
 
 const HeroBanner = ({ HeroBanner }) => {
     return (
         <div className="hero-banner-container">
             <div>
-                <p className="beats-solo">{HeroBanner?.smallText || "blah blah blah"}</p>
-                <h3>{HeroBanner?.midText || "blah blah blah"}</h3>
-                <h1>{HeroBanner?.largeText1 || "blah blah blah"}</h1>
-                {/* <img src={urlFor(HeroBanner?.image) || "blah blah blah"} alt="image" className="hero-banner-image" /> */}
-                <img src={"/Data/dummy.png"} alt="image" className="hero-banner-image" />
-                {/* <Image width={450} height={450} src="/Data/dummy.png" alt="image" className="hero-banner-image" /> */}
-
+                <p className="beats-solo">{HeroBanner?.SmallText || "blah blah blah"}</p>
+                <h3>{HeroBanner?.MidText || "blah blah blah"}</h3>
+                <h1>{HeroBanner?.LargeText1 || "blah blah blah"}</h1>
+                <img src={UrlFor(HeroBanner?.Image)} alt="image" className="hero-banner-image" />
                 <div>
-                    <Link href={`/Product/${HeroBanner?.product || "blah blah blah"}`}>
-                        <button type="button">{HeroBanner?.buttonText || "blah blah blah"}</button>
+                    <Link href={`/Product/${HeroBanner?.Product || "blah blah blah"}`}>
+                        <button type="button">{HeroBanner?.ButtonText || "blah blah blah"}</button>
                     </Link>
                     <div className="desc">
                         <h5>Description</h5>
-                        <p>{HeroBanner?.desc || "blah blah blah"}</p>
+                        <p>{HeroBanner?.Desc || "blah blah blah"}</p>
                     </div>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
+import { Client } from '@/Utilities/Client';
 import { FooterBanner, HeroBanner } from '@/Components/Index';
 import Product from './Product';
 
-const Home = () => {
+const Home = ({ Products, BannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner HeroBanner={BannerData.length && BannerData[0]} />
       <div className="products-heading">
         <h2>Best Seller Goods</h2>
         <p>Goods that most our Customers buy.</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2", "Product 12"]?.map((product) => product)}
+        {Products?.map((product) => product.Name)}
       </div>
       <FooterBanner />
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const Query = '*[_type == "Product"]';
+  const Products = await Client.fetch(Query);
+
+  const BannerQuery = '*[_type == "Banner"]';
+  const BannerData = await Client.fetch(BannerQuery);
+
+  return {
+    props: { Products, BannerData }
+  }
+}
+
 export default Home
