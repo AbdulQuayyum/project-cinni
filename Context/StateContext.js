c5d5a4c911be2053cea9ec73d9be9fcab27d2647
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-06 16:52:15 +0100

    Building Context...

--- Context/StateContext.js
+++ Context/StateContext.js
@@ -4,7 +4,47 @@ import { toast } from 'react-hot-toast';
 const Context = createContext()
 
 export const StateContext = ({ children }) => {
+    const [ShowCart, setShowCart] = useState(false);
+    const [CartItems, setCartItems] = useState([]);
+    const [TotalPrice, TetTotalPrice] = useState(0);
+    const [TotalQuantities, setTotalQuantities] = useState(0);
+    const [Qty, setQty] = useState(1);
 
+    let FoundProduct;
+    let Index;
+
+    const OnAdd = (Product, Quantity) => { }
+
+    const OnRemove = (Product) => { }
+
+    const ToggleCartItemQuantity = (ID, Value) => { }
+
+    const IncreaseQuantity = () => { }
+
+    const DecreaseQuantity = () => { }
+
+    return (
+        <Context.Provider
+            value={{
+                ShowCart,
+                setShowCart,
+                CartItems,
+                setCartItems,
+                TotalPrice,
+                TetTotalPrice,
+                TotalQuantities,
+                setTotalQuantities,
+                Qty,
+                setQty,
+                OnAdd,
+                OnRemove,
+                ToggleCartItemQuantity,
+                IncreaseQuantity,
+                DecreaseQuantity
+            }}>
+            {children}
+        </Context.Provider>
+    )
 }
 
-export const useStateContext = useContext(Context)
+export const useStateContext = () => useContext(Context);
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
+import { StateContext } from '@/Context/StateContext';
 import MainLayout from '@/Layout/Main.Layout';
 import '../Styles/Index.css'
 import '../Styles/Style.css'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
+    <StateContext>
       <MainLayout>
         <Toaster />
         <Component {...pageProps} />
       </MainLayout>
-    </>
+    </StateContext>
   )
 }
 
