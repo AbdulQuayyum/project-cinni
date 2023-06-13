b824901e940bf6c06feea0050e68cda48a8d0a4d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-13 15:50:29 +0100

    Working on the Search Page...

--- pages/Search/[SearchTerm].js
+++ pages/Search/[SearchTerm].js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-const SearchTerm = () => {
+export default function SearchTerm() {
+    const router = useRouter()
+    const { Category = 'all', Query = 'all', Price = 'all', Rating = 'all', Sort = 'default' } = router.query;
+    const [state, setState] = useState({ Categories: [], Products: [], Error: '', Loading: true })
+
+    const { Loading, Products, Error } = state;
+    const [Categories, setCategories] = useState([]);
+
     return (
         <div>SearchTerm</div>
     )
 }
-
-export default SearchTerm
