c4e8a084223346d7e05eaadc07f533f71a9275e9
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-13 03:11:35 +0100

    Working on the Order History Page...

---
+++ pages/OrderHistory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import MainLayout from '@/Layout/Main.Layout'
+
+const OrderHistory = () => {
+    return (
+        <MainLayout>OrderHistory</MainLayout>
+    )
+}
+
+export default OrderHistory
