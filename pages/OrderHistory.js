dc94798517254d79bc4b13e9e76b2bc7c4ecc5b3
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-13 03:32:33 +0100

    Working on the Order History Page...

--- pages/OrderHistory.js
+++ pages/OrderHistory.js
@@ -6,7 +6,7 @@ import MainLayout from '@/Layout/Main.Layout'
 const OrderHistory = () => {
     return (
         <MainLayout>
-            <div className='flex flex-col gap-y-2 w-full'>
+            <div className='flex flex-col gap-y-2 px-4 sm:px-8 mt-10 w-full'>
                 <span className='price'>Your Order History</span>
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -20,11 +20,11 @@ const OrderHistory = () => {
                     </thead>
                     <tbody>
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td className=" px-2 sm:px-6 py-4">qwertyuiop</td>
-                            <td className=" px-2 sm:px-6 py-4">	2023-06-13T01:58:56.766Z</td>
-                            <td className=" px-2 sm:px-6 py-4">₦10</td>
-                            <td className=" px-2 sm:px-6 py-4">Not Paid</td>
-                            <td className=" px-2 sm:px-6 py-4">
+                            <td className=" px-2 sm:px-6 py-8">qwertyuiop</td>
+                            <td className=" px-2 sm:px-6 py-8">	2023-06-13T01:58:56.766Z</td>
+                            <td className=" px-2 sm:px-6 py-8">₦10</td>
+                            <td className=" px-2 sm:px-6 py-8">Not Paid</td>
+                            <td className=" px-2 sm:px-6 py-8">
                                 <Link
                                     href={`Order/`}>
                                     <span
