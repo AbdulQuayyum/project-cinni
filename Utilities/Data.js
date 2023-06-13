1508b9f8d62921a1eac66f8fe8d2669a560ab124
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-13 18:24:55 +0100

    Added an object of categories...

---
+++ Utilities/Data.js
@@ -0,0 +1,55 @@
+import { CgGym } from 'react-icons/cg'
+import { FaBowlFood } from 'react-icons/fa'
+import { GiConverseShoe } from 'react-icons/gi'
+import { RiTruckLine } from 'react-icons/ri'
+import { AiOutlineFormatPainter } from 'react-icons/ai'
+import { MdOutlineCarpenter, MdPlumbing } from 'react-icons/md'
+import { LuGlassWater, LuShirt } from 'react-icons/lu'
+
+export const Catgories = [
+    {
+        name: 'Food',
+        type: 'Goods',
+        icon: <FaBowlFood />
+    },
+    {
+        name: 'Water',
+        type: 'Goods',
+        icon: <LuGlassWater />
+    },
+    {
+        name: 'Clothes',
+        type: 'Goods',
+        icon: <LuShirt />
+    },
+    {
+        name: 'Shoes',
+        type: 'Goods',
+        icon: <GiConverseShoe />
+    },
+    {
+        name: 'Movers',
+        type: 'services',
+        icon: <RiTruckLine />
+    },
+    {
+        name: 'Gym Instructor',
+        type: 'services',
+        icon: <CgGym />
+    },
+    {
+        name: 'Carpenter',
+        type: 'services',
+        icon: <MdOutlineCarpenter />
+    },
+    {
+        name: 'Plumber',
+        type: 'services',
+        icon: <MdPlumbing />
+    },
+    {
+        name: 'Painter',
+        type: 'services',
+        icon: <AiOutlineFormatPainter />
+    },
+]
