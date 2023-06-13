af438443f0319ed3210cc155a57858996ba956dc
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-13 17:37:42 +0100

    Adding some of our features to the project..

--- Components/Features.jsx
+++ Components/Features.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { ImHeadphones } from 'react-icons/im'
+import { BsPatchCheck } from 'react-icons/bs'
+import { IoRocketOutline } from 'react-icons/io5'
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='flex w-full justify-around flex-wrap gap-8 text-[#aaa] mt-8'>
+            <div className='flex border border-[#aaa] py-4 px-8 rounded-xl gap-x-6 items-center'>
+                <div>
+                    <IoRocketOutline size={40} />
+                </div>
+                <div className='flex flex-col gap-y-2'>
+                    <span className='font-bold'>Fast Delivery</span>
+                    <span className='font-lighter'>Fast Delivery on all orders</span>
+                </div>
+            </div>
+            <div className='flex border border-[#aaa] py-4 px-8 rounded-xl gap-x-6 items-center'>
+                <div>
+                    <BsPatchCheck size={40} />
+                </div>
+                <div className='flex flex-col gap-y-2'>
+                    <span className='font-bold'>100% Trust</span>
+                    <span className='font-lighter'>Business & Customer Trust</span>
+                </div>
+            </div>
+            <div className='flex border border-[#aaa] py-4 px-8 rounded-xl gap-x-6 items-center'>
+                <div>
+                    <ImHeadphones size={40} />
+                </div>
+                <div className='flex flex-col gap-y-2'>
+                    <span className='font-bold'>Customer Support</span>
+                    <span className='font-lighter'>24/7 Customer services</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import MainLayout from '@/Layout/Main.Layout';
 import { Client } from '@/Utilities/Client';
-import { FooterBanner, HeroBanner } from '@/Components/Index';
+import { Features, FooterBanner, HeroBanner } from '@/Components/Index';
 import Product from './Product';
 
 const Home = ({ products, bannerData }) => {
   return (
     <MainLayout>
       <HeroBanner HeroBanner={bannerData.length && bannerData[0]} />
+      <Features />
       <div className="products-heading">
         <h2>Best Seller Goods</h2>
         <p>Goods that most our Customers buy.</p>
