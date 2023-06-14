783763dc4d84625f83a0137c2ca9eea13c80db43
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-14 23:52:42 +0100

    removed unneccesary codes...

--- pages/api/Information.js
+++ pages/api/Information.js
@@ -1,15 +1,6 @@
 import { setCookie, getCookie } from 'cookies-next';
 
 export default function handler(req, res) {
-    const options = {
-        // secure: true, // Set the Secure flag
-        // httpOnly: true,
-        maxAge: 60 * 60 * 24 * 7, // Expires in 7 days
-        secure: true, // Only send the cookie over HTTPS
-        path: '/', // Set the cookie for the entire domain
-        sameSite: 'strict', // Restrict the cookie to same-site requests
-    };
-
 
     if (req.method === 'GET') {
         const UserAddress = getCookie('123456', { req, res })
@@ -20,7 +11,7 @@ export default function handler(req, res) {
     } else if (req.method === 'POST') {
         const { UserAddress } = req.body
 
-        setCookie('123456', JSON.stringify(UserAddress), { req, res, options });
+        setCookie('123456', JSON.stringify(UserAddress), { req, res, maxAge: 60 * 60 * 24 * 7 });
         res.status(200).json({ message: 'data saved successfully' });
 
     } else {
