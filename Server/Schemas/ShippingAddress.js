07e8ef762e8c1bb889b7b21fe01493aa4324f072
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-16 02:04:34 +0100

    Added PAYMENT METHOOD schema to the project...

---
+++ Server/Schemas/ShippingAddress.js
@@ -0,0 +1,27 @@
+export default {
+    title: 'Shipping Address',
+    name: 'shippingAddress',
+    type: 'object',
+    fields: [
+        {
+            title: 'Address',
+            name: 'Address',
+            type: 'string',
+        },
+        {
+            title: 'Landmark',
+            name: 'Landmark',
+            type: 'string',
+        },
+        {
+            title: 'Phone',
+            name: 'Phoone',
+            type: 'number'
+        },
+        {
+            title: 'country',
+            name: 'country',
+            type: 'string',
+        },
+    ],
+};
