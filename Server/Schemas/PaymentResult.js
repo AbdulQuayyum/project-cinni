15190b21840b996306919fb90a19f783d803b96e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-16 02:00:17 +0100

    Added PAYMENT METHOOD schema to the project...

---
+++ Server/Schemas/PaymentResult.js
@@ -0,0 +1,22 @@
+export default {
+    title: 'PaymentResult',
+    name: 'PaymentResult',
+    type: 'object',
+    fields: [
+        {
+            title: 'ID',
+            name: 'ID',
+            type: 'string',
+        },
+        {
+            title: 'Status',
+            name: 'Status',
+            type: 'string',
+        },
+        {
+            title: 'Email_address',
+            name: 'Email_address',
+            type: 'string',
+        },
+    ],
+};
