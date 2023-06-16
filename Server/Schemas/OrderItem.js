91787a3bb0af8e481198e2287a5dce1cf4541e0b
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-16 01:58:29 +0100

    Added OrderItem schema to the project...

---
+++ Server/Schemas/OrderItem.js
@@ -0,0 +1,27 @@
+export default {
+    title: 'Order Item',
+    name: 'orderItem',
+    type: 'object',
+    fields: [
+        {
+            title: 'Name',
+            name: 'Name',
+            type: 'string',
+        },
+        {
+            title: 'Quantity',
+            name: 'Quantity',
+            type: 'number',
+        },
+        {
+            title: 'Image',
+            name: 'Image',
+            type: 'string',
+        },
+        {
+            title: 'Price',
+            name: 'Price',
+            type: 'number',
+        },
+    ],
+};
