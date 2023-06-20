6de04ec24819a396eb148f26b2615fee89ad7362
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-20 14:39:20 +0100

    Removed a shema i'm not using...

--- Server/Schemas/Index.js
+++ Server/Schemas/Index.js
@@ -1,5 +1,4 @@
 import Banner from "./Banner"
-import Category from "./Category"
 import Comment from "./Comment"
 import Order from "./Order"
 import OrderItem from "./OrderItem"
@@ -10,4 +9,4 @@ import Save from "./Save"
 import ShippingAddress from "./ShippingAddress"
 import User from "./User"
 
-export const schemaTypes = [Banner, Category, Comment, Order, OrderItem, PaymentResult, PostedBy, Product, Save, ShippingAddress, User]
+export const schemaTypes = [Banner, Comment, Order, OrderItem, PaymentResult, PostedBy, Product, Save, ShippingAddress, User]
