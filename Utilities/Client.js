2d56040543574a4a0db068f90f801ff009766153
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-20 15:26:08 +0100

    Editing project...

--- Utilities/Client.js
+++ Utilities/Client.js
@@ -2,11 +2,12 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const Client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    ignoreBrowserTokenWarning: true,
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     useCdn: true,
-    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(Client)
